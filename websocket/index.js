2b78c8154ca81db4cb68d31fa37ae3adb101dce2
ioshe <sdcard11291@gmail.com>
2024-03-24 22:09:07 +0900

    feat 채팅 메세지를 전부에게 표시하는 기능 추가

--- websocket/index.js
+++ websocket/index.js
@@ -12,10 +12,10 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
